892b04fa6aef6cbc0846c6e0d85ce7e144d67e9d
Andrea Cedraro <a.cedraro@gmail.com>
2020-11-01 15:27:49 +0100

    Drop custom made treesitter debug layer.
    Use https://github.com/nvim-treesitter/playground instead.

--- lua/ycm.lua
+++ lua/ycm.lua
@@ -13,10 +13,6 @@
 
 local autocmd = require('ycm.autocmd')
 
--- XXX(andrea): this upper case M is really bugging me. Find a proper name.
--- Maybe just `ycm`? or `client` as in a client for `ycmd`?
-local M = {}
-
 -- XXX(andrea): find out if there is a better way in lua.
 local plugin_directory = vim.fn.fnamemodify(vim.fn.resolve(vim.fn.expand('<sfile>:p')), ':h:h')
 
@@ -119,7 +115,7 @@ local function collect_and_send_refresh_identifiers()
   vim.rpcnotify(remote_job_id, "refresh_buffer_identifiers", ft, fp, identifiers)
 end
 
-function M.show_candidates(id, cands)
+local function show_candidates(id, cands)
   -- throw away results for our of date requests
   if complete_id ~= id then
     return
@@ -309,114 +305,6 @@ local function complete_p()
   complete()
 end
 
-
-
--- Debug layer
-
-local debughl_ns = vim.api.nvim_create_namespace("ycmtsdebughl")
-
-local function highlight(bufnr, line, col_start, col_end)
-  vim.api.nvim_buf_add_highlight(bufnr, debughl_ns, "WildMenu", line, col_start, col_end)
-end
-local function highlight_node(bufnr, node)
-  local start_row, start_col, end_row, end_col = node:range()
-
-  vim.api.nvim_buf_clear_namespace(0, debughl_ns, 0, -1)
-  if start_row == end_row then
-    highlight(bufnr, start_row, start_col, end_col)
-  else
-    highlight(bufnr, start_row, start_col, -1)
-    for line = start_row + 1, end_row - 1 do
-      highlight(bufnr, line, 0, -1)
-    end
-    highlight(bufnr, end_row, 0, end_col)
-  end
-end
-
-local function print_identifier_at_cursor()
-  local bufnr = vim.api.nvim_get_current_buf()
-  local buffer = buffers[bufnr]
-
-  local row, col = get_position()
-  local tree = buffer:parse()
-
-  local at_cursor = tree:root():named_descendant_for_range(row, col, row, col)
-  if at_cursor ~= nil then
-    log("ts node type -> "..at_cursor:type())
-    highlight_node(bufnr, at_cursor)
-  end
-end
-
-local function nvim_clear_augroup(group)
-  vim.cmd("augroup "..group.." | autocmd! | augroup END")
-end
-
-M._CB = {}
-
-local function join(...)
-  return table.concat({...}, " ")
-end
-
-local function nvim_autocmd(definition)
-  local group = definition.group or "END"
-
-  local event = definition.event
-  if event == nil then
-    error("event is required")
-  end
-
-  local pat = "*"
-  local buffer = definition.buffer
-  if buffer ~= nil then
-    if type(buffer) == "number" then
-      pat = "<buffer="..buffer..">"
-    else
-      pat = "<buffer>"
-    end
-  end
-
-  local once = definition.once and "++once" or ""
-  local nested = definition.nested and "++nested" or ""
-
-  local cb = definition.cmd
-  if cb == nil then
-    error("cmd is required")
-  end
-
-  M._CB[cb] = cb
-  local cmd = "lua require'ycm'._CB["..tostring(cb).."]()"
-
-  vim.cmd("augroup "..group)
-  vim.cmd(join("autocmd ", event, pat, once, nested, cmd))
-  vim.cmd("augroup END")
-end
-
-function M.enable_ts_debug_layer_for_buffer()
-  if not allowed_in_buffer() then
-    return
-  end
-
-  nvim_clear_augroup("ycmtsdebug")
-  nvim_autocmd {
-    group = "ycmtsdebug",
-    event = "CursorMoved",
-    cmd = print_identifier_at_cursor,
-    buffer = true
-  }
-  nvim_autocmd {
-    group = "ycmtsdebug",
-    event = "InsertEnter",
-    cmd = M.disable_ts_debug_layer,
-    buffer = true
-  }
-  print_identifier_at_cursor()
-end
-
-function M.disable_ts_debug_layer()
-  nvim_clear_augroup("ycmtsdebug")
-  vim.api.nvim_buf_clear_namespace(0, debughl_ns, 0, -1)
-end
-
 local function setup_autocmds()
   autocmd.define_augroup('ycm', true)
   -- XXX(andrea): the FileType event should also handle the case where a
@@ -462,7 +350,7 @@ local function start_ycm()
 end
 
 
-function M.setup()
+local function setup()
   vim.o.completeopt = "menuone,noinsert,noselect"
   vim.cmd[[set shortmess+=c]]
 
@@ -471,4 +359,7 @@ function M.setup()
 end
 
 
-return M
+return {
+  setup = setup,
+  show_candidates = show_candidates
+}
